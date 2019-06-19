624815d1060a294aaddb64178981f2fa864c95e6
Iceman2002 <51944568+Iceman2002@users.noreply.github.com>
2019-06-19 14:23:48 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'FefYFphxqaWpl3iBm1nooA7WH4m3l8QKM6lpUVw97aDZG5xfbQ5iSZEzWg5dqJ1tkASIkfLjeyjgtyB6kW/GGqAiXl9mHX+8ZiZ7cPOW2a3brP4pQWZGr2acCyQlvMIxdM5cJyTGasnDbN+5a0ihEAdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
